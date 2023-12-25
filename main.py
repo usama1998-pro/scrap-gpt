44becc14eca83e0d8806c9b013c469af74d8b662
usama1998-pro <proattackers69@gmail.com>
2023-12-25 21:48:18 +0500

    chroma issue solved

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 from dotenv import load_dotenv
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
